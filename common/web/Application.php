19e76fc5dc56f5b784cc81a8322ba24a15e39da9
Qiang Li <joneleesz@hotmail.com>
2016-05-30 11:08:47 +0100

    add easier interface to call in view file....

--- common/web/Application.php
+++ common/web/Application.php
@@ -9,6 +9,11 @@
 namespace yii\common\web;
 
 
+use common\models\Banner;
+use common\models\HtmlBlock;
+use common\models\TextBlock;
+use funson86\blog\models\Status;
+
 class Application extends \yii\web\Application
 {
     protected $isBackend = null;
@@ -83,6 +88,60 @@ class Application extends \yii\web\Application
 
             //remove any theme from configuration file..
             $view->theme = null;
+        }
+    }
+
+    /****
+     * get banner or gallary from db, this could be commonly used by frontend code
+     *
+     * id can be  a id or can be name
+     * if pass a id list then it will return a group of banner otherwise, it will check a matched id or name
+     * eg. 	$banner = Yii::$app->getBanners(['1232', 'namd']);
+     */
+   public function getBanners($id){
+       if(!is_array($id)){
+           return Banner::find()->where(['status'=> Status::STATUS_ACTIVE])->andWhere(['or', ['id'=> $id], ['name'=>$id]])->one();
+       }
+       else {
+           return Banner::find()->where(['status'=> Status::STATUS_ACTIVE])->andwhere(['or', ['in', 'id', $id], ['in','name', $id]])->all();
+       }
+    }
+
+    /****
+     * get a group of banners from db, this could be commonly used by frontend code
+     *
+     * $groupid: just a group id or several group via array
+     * eg. 	$banner = Yii::$app->getBannerByGroup(123);
+     */
+    public function getBannerByGroup($groupid){
+        if(!is_array($groupid)){
+            return Banner::find()->where(['status'=> Status::STATUS_ACTIVE])->andwhere(['groupid'=> $groupid])->one();
+        } else {
+            return Banner::find()->where(['status'=> Status::STATUS_ACTIVE])->andwhere(['in', 'groupid', $groupid])->all();
+        }
+    }
+
+    /******
+     * as above
+     * 	$textbanner = Yii::$app->getTextBlock([1, 2, 3]);
+     */
+    public function getTextBlock($id){
+        if(!is_array($id)){
+            return TextBlock::find()->where(['or', ['id'=> $id, 'name'=>$id]])->one();
+        } else {
+            return TextBlock::find()->where(['or', ['in', 'id',  $id], ['in', 'name', $id]])->all();
+        }
+    }
+
+    /******
+     * as above
+     * 	Yii::$app->getHtmlBlock([5, 'test']);
+     */
+    public function getHtmlBlock($id){
+        if(!is_array($id)){
+            return HtmlBlock::find()->where(['or', ['id'=> $id, 'name'=>$id]])->one();
+        } else {
+            return HtmlBlock::find()->where(['or', ['in', 'id',  $id], ['in', 'name', $id]])->all();
         }
     }
 }
