201b704764c4d1a3f8ead2fba6e9d56343c038a4
Qiang Li <joneleesz@hotmail.com>
2016-06-07 23:05:49 +0100

    link twitter and Facebook

--- frontend/template/sports/layouts/footer.php
+++ frontend/template/sports/layouts/footer.php
@@ -35,13 +35,13 @@
                 </div>
 
                 <div class="col-md-3 widget">
-                    <h3 class="widget-title">Follow me</h3>
+                    <h3 class="widget-title">Follow us</h3>
                     <div class="widget-body">
                         <p class="follow-me-icons">
-                            <a href=""><i class="fa fa-twitter fa-2"></i></a>
-                            <a href=""><i class="fa fa-dribbble fa-2"></i></a>
-                            <a href=""><i class="fa fa-github fa-2"></i></a>
-                            <a href=""><i class="fa fa-facebook fa-2"></i></a>
+                            <a href="<?=Yii::$app->setting->get('twitter')?>"><i class="fa fa-twitter fa-2"></i></a>
+                            <!--a href="<?=Yii::$app->setting->get('twitter')?>"><i class="fa fa-dribbble fa-2"></i></a>
+                            <a href=""><i class="fa fa-github fa-2"></i></a-->
+                            <a href="<?=Yii::$app->setting->get('facebook')?>"><i class="fa fa-facebook fa-2"></i></a>
                         </p>
                     </div>
                 </div>
