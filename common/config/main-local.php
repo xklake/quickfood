908783d4e0fe18887a7e5b618a533c5ba034bc9c
Qiang Li <joneleesz@hotmail.com>
2016-07-01 11:13:03 +0100

    correct .htaccess files, always cause problem for us..

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=pandacms_sports',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=pandacms_mordern',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
