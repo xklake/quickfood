49fbb39bdfcda773c5cefc6662d177a83e425d6f
Qiang Li <joneleesz@hotmail.com>
2018-04-11 17:28:22 +0100

    refine the error page

--- views/error.php
+++ views/error.php
@@ -5,17 +5,16 @@
  * Date: 6/10/16
  * Time: 2:25 PM
  */
+$this->context->layout = 'column1';
 ?>
 
-<section id="error" class="container text-center" style="margin: 200px 0px;">
+<section id="error" class="container text-center" style="margin: 100px 0px;">
     <div class="container">
         <div class="row">
-            <h1>SOMETHING IS WRONG !!</h1>
+            <h1>Bad luck,something is not right...</h1>
             <p>The page does not exist, please check your url or contact the website owner.</p>
 
-            <span class="btn btn-success">
-                Back To <a  href="<?= Yii::$app->urlManager->getHostInfo() . Yii::$app->homeUrl ?>" style="margin-top: 20px;">Home Page</a>
-            <span>
+            <p>Back To <a  href="<?= Yii::$app->urlManager->getHostInfo() . Yii::$app->homeUrl ?>" style="margin-top: 20px;">Home Page</a></p>
         </div>
     </div>
 </section><!--/#error-->
